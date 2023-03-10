cf9da6824bed2d0b0698beb47fdb02441e7a8c75
JamJamAl <mohammadjamal@student.telkomuniversity.ac.id>
2023-03-10 10:46:29 +0700

    v1.0

---
+++ Tugas.py
@@ -0,0 +1,3 @@
+people = {1: {"nama": "arsene lupin", "nik": "1234", "jenis_kelamin": "male", "tanggal_lahir":"1902-03-23"},
+          2: {"nama": "sherlock holmes", "nik": "4321", "jenis_kelamin": "male", "tanggal_lahir":"1876-08-16"},
+          3: {"nama": "irene adler", "nik":"6789", "jenis_kelamin": "female", "tanggal_lahir":"1884-10-07"}}
